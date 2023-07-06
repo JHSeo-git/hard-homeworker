36f67ebab35c3b791800be5dcf880c4471515dd9
JHSeo <qnfqnfqnf@gmail.com>
2023-07-06 22:36:17 +0900

    chore

--- prisma/migrations/20230630143843_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "WaitList" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "url" TEXT NOT NULL,
-    "isDone" BOOLEAN NOT NULL DEFAULT false
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "WaitList_url_key" ON "WaitList"("url");
